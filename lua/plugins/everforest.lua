0dfec9981436e9222fbcd3592a434a4ffbee4030
abelsiby <98385134+abelsiby@users.noreply.github.com>
2024-11-26 22:17:31 +0530

    Update everforest.lua

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -5,5 +5,6 @@
   priority = 1000, -- make sure to load this before all the other start plugins
   -- Optional; default configuration will be used if setup isn't called.
   config = function()
+    vim.cmd([[colorscheme everforest]])
   end,
 }
