169829a9f395a1a9116343f464b9c5293d7d5364
Abel Siby <98385134+abelsiby@users.noreply.github.com>
2026-03-08 00:43:41 +0530

    Refactor Molten plugin configuration
    1. Fixed lazy loading conflict
    2. Unused variables cleanup

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -1,9 +1,8 @@
 return {
   "benlubas/molten-nvim",
   version = "^1.0.0",
-  lazy = false,
-  -- Load for python/markdown since you're using Jupytext/Notebooks
-  ft = { "python", "markdown", "quarto", "ipynb" },
+  -- Load specifically for data science filetypes (lazy = false removed)
+  ft = { "python", "markdown", "quarto", "ipynb" }, 
   build = ":UpdateRemotePlugins",
   init = function()
     -- These MUST be set before the plugin loads
@@ -16,23 +15,18 @@ return {
   end,
   config = function()
     -- Keymaps
-    local opts = { silent = true }
-    vim.keymap.set("n", "<leader>mi", "<cmd>MoltenInit<cr>",
-      { silent = true, desc = "Initialize Kernel" })
-    vim.keymap.set("n", "<leader>me", "<cmd>MoltenEvaluateOperator<cr>",
-      { silent = true, desc = "Run Operator" })
-    vim.keymap.set("n", "<leader>ml", "<cmd>MoltenEvaluateLine<cr>",
-      { silent = true, desc = "Run Line" })
-    vim.keymap.set("v", "<leader>mv", ":<C-u>MoltenEvaluateVisual<cr>gv",
-      { silent = true, desc = "Run Visual" })
-    vim.keymap.set("n", "<leader>mh", "<cmd>MoltenHideOutput<cr>",
-      { silent = true, desc = "Hide Output" })
+    vim.keymap.set("n", "<leader>mi", "<cmd>MoltenInit<cr>", { silent = true, desc = "Initialize Kernel" })
+    vim.keymap.set("n", "<leader>me", "<cmd>MoltenEvaluateOperator<cr>", { silent = true, desc = "Run Operator" })
+    vim.keymap.set("n", "<leader>ml", "<cmd>MoltenEvaluateLine<cr>", { silent = true, desc = "Run Line" })
+    vim.keymap.set("v", "<leader>mv", ":<C-u>MoltenEvaluateVisual<cr>gv", { silent = true, desc = "Run Visual" })
+    vim.keymap.set("n", "<leader>mh", "<cmd>MoltenHideOutput<cr>", { silent = true, desc = "Hide Output" })
+    
     -- Special handling for Notebook users
     -- Automatically import your Jupytext markdown code cells into Molten
     vim.keymap.set("n", "<leader>ip", function()
       local v = vim.fn.winsaveview()
       vim.cmd([[MoltenImportOutput]])
       vim.fn.winrestview(v)
-    end, { desc = "Import Notebook Output" })
+    end, { silent = true, desc = "Import Notebook Output" }) -- Added silent = true here for consistency
   end,
 }
